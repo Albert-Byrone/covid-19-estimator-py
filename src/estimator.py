223c3226a96c929b4e87000f1c928b6c5cc46382
albertbyrone <albertbyrone1677@gmail.coim>
2020-04-13 18:33:50 +0300

    add get_dollars_in_flight function

--- src/estimator.py
+++ src/estimator.py
@@ -50,5 +50,18 @@ def get_cases_for_ventilators_by_requested_time(output):
 
   output['severeImpact']['casesForVentilatorsByRequestedTime'] = int(0.02 * output['severeImpact']['infectionsByRequestedTime'])
 
+
+def get_dollars_in_flight(data, output):
+  population_perc = data['region']['avgDailyIncomePopulation']
+  population_inc = data['region']['avgDailyIncomeInUSD']
+  days = get_days(data['periodType'], data['timeToElapse'])
+
+  impact_infections = output['impact']['infectionsByRequestedTime']
+  output['impact']['dollarsInFlight'] = int((impact_infections * population_perc * population_inc) / days)
+
+  s_impact_infections = output['severeImpact']['infectionsByRequestedTime']
+  output['severeImpact']['dollarsInFlight'] = \
+      int((s_impact_infections * population_perc * population_inc) / days)
+
 def estimator(data):
   return data
