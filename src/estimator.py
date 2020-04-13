9331fd1ca3fa7d2663fe65df7f9bc5d4bcaf0303
albertbyrone <albertbyrone1677@gmail.coim>
2020-04-13 18:31:17 +0300

    add get_currently_infected_people

--- src/estimator.py
+++ src/estimator.py
@@ -8,5 +8,11 @@ def get_days(period, time_elapsed):
   if period == 'months':
       return time_elapsed * 30
 
+def get_currently_infected_people(data, output):
+  reported_cases = data['reportedCases']
+  output['impact']['currentlyInfected'] = reported_cases * 10
+  output['severeImpact']['currentlyInfected'] = reported_cases * 50
+
+
 def estimator(data):
   return data
