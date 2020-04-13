64059d48b5140e1b3684c898558945b4ce3d2503
albertbyrone <albertbyrone1677@gmail.coim>
2020-04-13 18:29:46 +0300

    add a function to return the time bases on the period

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,12 @@
+def get_days(period, time_elapsed):
+  if period == 'days':
+      return time_elapsed
+
+  if period == 'weeks':
+      return time_elapsed * 7
+
+  if period == 'months':
+      return time_elapsed * 30
+
 def estimator(data):
   return data
